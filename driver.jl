17a1614db22ac1b2674adeae8cc1fb08ea043904
yhz <35647076+miaonima@users.noreply.github.com>
2022-12-08 13:31:11 +0800

    added bcd plot

--- driver.jl
+++ driver.jl
@@ -12,16 +12,20 @@ K, rel_error = gd(mygrad; max_iter=300, true_cost = C_star)
 K2, rel_error2 = gd(mygrad_twoside; max_iter=150, true_cost = C_star)
 
 # # Plot results
-plot((1:300)*2000, rel_error, yaxis=:log, label="One Side")
-plot!((1:150)*4000, rel_error2, yaxis=:log, label="Two Sides")
-xlabel!("Number of Zeroth Order Oracle Calls")
+plot(1:300, rel_error, yaxis=:log, label="One Side")
+plot!(collect(1:2:300), rel_error2, yaxis=:log, label="Two Sides")
+xlabel!("Computational Effort (GD Iterations)")
 ylabel!("Relative Error")
+savefig("twoside.png")
 
-
+# Block coordinate descent
 K3, rel_error3 = bcd(mygrad; true_cost = C_star, max_iter=300)
-plot!((1:3000)*200, rel_error3, yaxis=:log)
+plot(1:300, rel_error, yaxis=:log, label="One Side")
+plot!(collect(0.1:0.1:300), rel_error3, yaxis=:log, label="Cyclic BCD")
+xlabel!("Computational Effort (GD Iterations)")
+ylabel!("Relative Error")
+savefig("bcd.png")
 
-xlims!(0.0, 3e5)
-
-K4, rel_error4 = bcd(mygrad_twoside; true_cost = C_star, max_iter=150)
-plot!((1:1500)*400, rel_error4, yaxis=:log)
+# two sided + bcd
+# K4, rel_error4 = bcd(mygrad_twoside; true_cost = C_star, max_iter=150)
+# plot!((1:1500)*400, rel_error4, yaxis=:log)
